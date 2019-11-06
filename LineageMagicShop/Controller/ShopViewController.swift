5132b8c5e871af8ae2f16329ef07e3242e37858b
ABlackTaco <ablacktaco@gmail.com>
2019-11-06 09:58:58 +0800

    git add .

--- LineageMagicShop/Controller/ShopViewController.swift
+++ LineageMagicShop/Controller/ShopViewController.swift
@@ -12,7 +12,14 @@ class ShopViewController: UIViewController, UITableViewDataSource, UITableViewDe
     
     let userData = UserData.shared
     
+    var index: Int = 0
+    
     @IBOutlet var mainView: UIView!
+    @IBOutlet var levelOneButton: UIButton!
+    @IBOutlet var levelTwoButton: UIButton!
+    @IBOutlet var levelThreeButton: UIButton!
+    @IBOutlet var switchTableViewButton: UIButton!
+    @IBOutlet var switchCollectionViewButton: UIButton!
     @IBOutlet var userMoney: UILabel!
     @IBOutlet var displayOfTable: UITableView!
     @IBOutlet var displayOfCollection: UICollectionView!
@@ -20,17 +27,35 @@ class ShopViewController: UIViewController, UITableViewDataSource, UITableViewDe
     @IBOutlet var alertView: UIView!
     @IBOutlet var purchaseView: UIView!
     @IBOutlet var moneyNotEnoughView: UIView!
+    @IBOutlet var magicIcon: UIImageView!
+    @IBOutlet var magicPrice: UILabel!
+    @IBOutlet var purchaseMagicButton: UIButton!
+    @IBOutlet var cancelPurchaseButton: UIButton!
+    @IBOutlet var byebyeButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationController?.navigationBar.tintColor = .white
+        userData.levelTag = 0
+        levelOneButton.backgroundColor = .lead
+        switchTableViewButton.backgroundColor = .lead
+        displayOfTable.separatorInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        displayOfCollection.isHidden = true
+        alertView.isHidden = true
+        settingAlertView()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        userMoney.text = "$ \(userData.userMoney)"
+    }
+    
+    // MARK: - Setting TableView
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return userData.magicData[userData.levelTag].count
-    }
-    
-    fileprivate func judgePurchaseState(_ data: MagicData, _ cell: ShopTableViewCell) {
-        if data.purchaseState {
-            cell.soldOutView.isHidden = false
-        } else {
-            cell.soldOutView.isHidden = true
-        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -42,10 +67,26 @@ class ShopViewController: UIViewController, UITableViewDataSource, UITableViewDe
         cell.magicIcon?.image = UIImage(named: data.magicName)
         cell.magicName.text = data.magicName
         cell.magicPrice.text = "$ \(data.magicPrice)"
-        judgePurchaseState(data, cell)
+        if data.purchaseState {
+            cell.soldOutView.isHidden = false
+        } else {
+            cell.soldOutView.isHidden = true
+        }
         
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        if userData.magicData[userData.levelTag][indexPath.row].purchaseState { return nil }
+        return indexPath
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        index = indexPath.row
+        showPurchaseView(indexPath)
+    }
+    
+    // MARK: - Setting CollectionView
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return userData.magicData[userData.levelTag].count
@@ -71,34 +112,89 @@ class ShopViewController: UIViewController, UITableViewDataSource, UITableViewDe
         return cell
     }
     
-    func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
-        if userData.magicData[userData.levelTag][indexPath.row].purchaseState {
-            return nil
-        }
-        return indexPath
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        index = indexPath.row
+        showPurchaseView(indexPath)
     }
     
-    var index: Int = 0
-    @IBOutlet var magicIcon: UIImageView!
-    @IBOutlet var magicPrice: UILabel!
-    @IBOutlet var purchaseMagicButton: UIButton!
+    // MARK: - ShopView Action
+    
+    @IBAction func ShowLevelOneMagic(_ sender: UIButton) {
+        userData.levelTag = 0
+        resetLevelButtonColor(sender)
+        reloadTableOrCollection()
+    }
+    @IBAction func ShowLevelTwoMagic(_ sender: UIButton) {
+        userData.levelTag = 1
+        resetLevelButtonColor(sender)
+        reloadTableOrCollection()
+    }
+    @IBAction func ShowLevelThreeMagic(_ sender: UIButton) {
+        userData.levelTag = 2
+        resetLevelButtonColor(sender)
+        reloadTableOrCollection()
+    }
+    
+    @IBAction func ShowTableViewState(_ sender: UIButton) {
+        displayOfCollection.isHidden = true
+        displayOfTable.isHidden = false
+        resetViewButtonColor(sender)
+        displayOfTable.reloadData()
+    }
+    @IBAction func ShowCollectionViewState(_ sender: UIButton) {
+        displayOfTable.isHidden = true
+        displayOfCollection.isHidden = false
+        resetViewButtonColor(sender)
+        displayOfCollection.reloadData()
+    }
+    
+    // MARK: - AlertView Action
+    
     @IBAction func toPurchaseMagic(_ sender: UIButton) {
         userData.userMoney -= userData.magicData[userData.levelTag][index].magicPrice
         userData.magicData[userData.levelTag][index].purchaseState.toggle()
-        alertView.isHidden = true
-        mainView.isUserInteractionEnabled = true
+        hideAlertView()
         viewWillAppear(false)
         reloadTableOrCollection()
     }
-    @IBOutlet var cancelPurchaseButton: UIButton!
     @IBAction func cancelPurchaseMagic(_ sender: UIButton) {
-        alertView.isHidden = true
-        mainView.isUserInteractionEnabled = true
+        hideAlertView()
     }
-    @IBOutlet var byebyeButton: UIButton!
     @IBAction func byebyeAction(_ sender: UIButton) {
-        alertView.isHidden = true
-        mainView.isUserInteractionEnabled = true
+        hideAlertView()
+    }
+    
+    // MARK: - Addition Function
+    
+    fileprivate func resetLevelButtonColor(_ sender: UIButton) {
+        levelOneButton.backgroundColor = .darkGray
+        levelTwoButton.backgroundColor = .darkGray
+        levelThreeButton.backgroundColor = .darkGray
+        sender.backgroundColor = .lead
+    }
+    
+    fileprivate func resetViewButtonColor(_ sender: UIButton) {
+        switchTableViewButton.backgroundColor = .darkGray
+        switchCollectionViewButton.backgroundColor = .darkGray
+        sender.backgroundColor = .lead
+    }
+    
+    fileprivate func reloadTableOrCollection() {
+        if displayOfTable.isHidden {
+            displayOfCollection.reloadData()
+        } else {
+            displayOfTable.reloadData()
+        }
+    }
+    
+    fileprivate func settingAlertView() {
+        alertView.addSubview(purchaseView)
+        purchaseView.center = CGPoint(x: alertView.frame.width / 2, y: alertView.frame.height / 2)
+        alertView.addSubview(moneyNotEnoughView)
+        moneyNotEnoughView.center = CGPoint(x: alertView.frame.width / 2, y: alertView.frame.height / 2)
+        set(button: purchaseMagicButton, configSetting: .purchaseButtonSetting)
+        set(button: cancelPurchaseButton, configSetting: .purchaseButtonSetting)
+        set(button: byebyeButton, configSetting: .purchaseButtonSetting)
     }
     
     fileprivate func showPurchaseView(_ indexPath: IndexPath) {
@@ -116,99 +212,10 @@ class ShopViewController: UIViewController, UITableViewDataSource, UITableViewDe
         }
     }
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        index = indexPath.row
-        showPurchaseView(indexPath)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        index = indexPath.row
-        showPurchaseView(indexPath)
-    }
-    
-    fileprivate func settingAlertView() {
-        alertView.addSubview(purchaseView)
-        purchaseView.center = CGPoint(x: alertView.frame.width / 2, y: alertView.frame.height / 2)
-        alertView.addSubview(moneyNotEnoughView)
-        moneyNotEnoughView.center = CGPoint(x: alertView.frame.width / 2, y: alertView.frame.height / 2)
-        set(button: purchaseMagicButton, configSetting: .purchaseButtonSetting)
-        set(button: cancelPurchaseButton, configSetting: .purchaseButtonSetting)
-        set(button: byebyeButton, configSetting: .purchaseButtonSetting)
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        userData.levelTag = 0
-        levelOneButton.backgroundColor = UIColor(red: 33.0/255.0, green: 33.0/255.0, blue: 33.0/255.0, alpha: 1.0)
-        switchTableViewButton.backgroundColor = UIColor(red: 33.0/255.0, green: 33.0/255.0, blue: 33.0/255.0, alpha: 1.0)
-        
-        displayOfTable.separatorInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-        displayOfCollection.isHidden = true
+    fileprivate func hideAlertView() {
         alertView.isHidden = true
-        settingAlertView()
-        navigationController?.navigationBar.tintColor = .white
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        userMoney.text = "$ \(userData.userMoney)"
-    }
-    
-    fileprivate func reloadTableOrCollection() {
-        if displayOfTable.isHidden {
-            displayOfCollection.reloadData()
-        } else {
-            displayOfTable.reloadData()
-        }
-    }
-    
-    fileprivate func resetLevelButtonColor(_ sender: UIButton) {
-        levelOneButton.backgroundColor = .darkGray
-        levelTwoButton.backgroundColor = .darkGray
-        levelThreeButton.backgroundColor = .darkGray
-        sender.backgroundColor = UIColor(red: 33.0/255.0, green: 33.0/255.0, blue: 33.0/255.0, alpha: 1.0)
-    }
-    
-    @IBOutlet var levelOneButton: UIButton!
-    @IBAction func ShowLevelOneMagic(_ sender: UIButton) {
-        userData.levelTag = 0
-        resetLevelButtonColor(sender)
-        reloadTableOrCollection()
-    }
-    @IBOutlet var levelTwoButton: UIButton!
-    @IBAction func ShowLevelTwoMagic(_ sender: UIButton) {
-        userData.levelTag = 1
-        resetLevelButtonColor(sender)
-        reloadTableOrCollection()
-    }
-    @IBOutlet var levelThreeButton: UIButton!
-    @IBAction func ShowLevelThreeMagic(_ sender: UIButton) {
-        userData.levelTag = 2
-        resetLevelButtonColor(sender)
-        reloadTableOrCollection()
-    }
-    
-    fileprivate func resetViewButtonColor(_ sender: UIButton) {
-        switchTableViewButton.backgroundColor = .darkGray
-        switchCollectionViewButton.backgroundColor = .darkGray
-        sender.backgroundColor = UIColor(red: 33.0/255.0, green: 33.0/255.0, blue: 33.0/255.0, alpha: 1.0)
-    }
-    
-    @IBOutlet var switchTableViewButton: UIButton!
-    @IBAction func ShowTableViewState(_ sender: UIButton) {
-        displayOfCollection.isHidden = true
-        displayOfTable.isHidden = false
-        resetViewButtonColor(sender)
-        displayOfTable.reloadData()
-    }
-    @IBOutlet var switchCollectionViewButton: UIButton!
-    @IBAction func ShowCollectionViewState(_ sender: UIButton) {
-        displayOfTable.isHidden = true
-        displayOfCollection.isHidden = false
-        resetViewButtonColor(sender)
-        displayOfCollection.reloadData()
+        mainView.isUserInteractionEnabled = true
     }
     
 }
+
---
+++ LineageMagicShop/Model/UIColor+Ext.swift
@@ -0,0 +1,15 @@
+//
+//  UIColor+Ext.swift
+//  LineageMagicShop
+//
+//  Created by 陳姿穎 on 2019/11/6.
+//  Copyright © 2019 陳姿穎. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    open class var lead: UIColor {
+        UIColor(red: 33.0/255.0, green: 33.0/255.0, blue: 33.0/255.0, alpha: 1.0)
+    }
+}
